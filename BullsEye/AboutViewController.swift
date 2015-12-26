a2233d4013f4bd52b13c1409ae01eeee72ce0674
Serghei Grigoruta <phoenix92@inbox.ru>
2015-12-26 18:22:25 +0200

    Initial Commit

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Serghei Grigoruta on 11/2/15.
+//  Copyright © 2015 Serghei Grigoruta. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+            if let htmlData = NSData(contentsOfFile: htmlFile) {
+                let baseUrl = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath)
+                webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseUrl)
+            }
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
